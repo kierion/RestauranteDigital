f369359dfd9afe49ebbbb98ddb4a4af281dcb165
Édipo <bob_edipo@hotmail.com>
2017-07-01 18:06:30 -0300

    Initial commit

---
+++ app/src/main/java/trabalhofinal/usj/com/br/restaurantedigital/RestauranteDigitalActivity.java
@@ -0,0 +1,14 @@
+package trabalhofinal.usj.com.br.restaurantedigital;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class RestauranteDigitalActivity extends Activity {
+
+}
